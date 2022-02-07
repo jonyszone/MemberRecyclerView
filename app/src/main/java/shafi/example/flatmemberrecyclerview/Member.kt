7277b6b0aa17854c387758a5a95dcd3354c7ce1a
jonyszone <jonyszone@gmail.com>
2022-02-07 17:25:06 +0600

    changed member activity from java to kotlin

--- app/src/main/java/shafi/example/flatmemberrecyclerview/Member.kt
+++ app/src/main/java/shafi/example/flatmemberrecyclerview/Member.kt
@@ -1,44 +1,18 @@
-package shafi.example.flatmemberrecyclerview;
+package shafi.example.flatmemberrecyclerview
 
-import java.util.ArrayList;
+import shafi.example.flatmemberrecyclerview.R
+import java.util.ArrayList
 
-public class Member {
-    private final int image;
-    private final String name;
-    private final String phoneNo;
-    private final String email;
+class Member(val image: Int, val name: String, val phoneNo: String, val email: String) {
 
-    public Member(int image, String name, String phoneNo, String email) {
-        this.image = image;
-        this.name = name;
-        this.phoneNo = phoneNo;
-        this.email = email;
-    }
-
-    public int getImage() {
-        return image;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhoneNo() {
-        return phoneNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public static ArrayList<Member> memberArrayList(){
-        ArrayList<Member> members = new ArrayList<>();
-        members.add(new Member(R.drawable.member,"Shafi","01245555445","ip@gmail.com"));
-        members.add(new Member(R.drawable.member2,"Tawheed","01252245","tw@gmail.com"));
-        members.add(new Member(R.drawable.member3,"Ghalib","01254564","gh@gmaili.com"));
-        members.add(new Member(R.drawable.member4,"Mehedi","01245365","md@gmali.com"));
-        return members;
+    companion object {
+        fun memberArrayList(): ArrayList<Member> {
+            val members = ArrayList<Member>()
+            members.add(Member(R.drawable.member, "Shafi", "01245555445", "ip@gmail.com"))
+            members.add(Member(R.drawable.member2, "Tawheed", "01252245", "tw@gmail.com"))
+            members.add(Member(R.drawable.member3, "Ghalib", "01254564", "gh@gmaili.com"))
+            members.add(Member(R.drawable.member4, "Mehedi", "01245365", "md@gmali.com"))
+            return members
+        }
     }
 }
-
-
