a79a19928745a3b0be72d0717e412ca2cd25f65d
Shafi <jonyszone@gmail.com>
2023-10-08 08:21:12 +0600

    refracted some code in main activity

---
+++ app/src/main/java/shafi/example/flatmemberrecyclerview/HomeFragment.kt
@@ -0,0 +1,6 @@
+package shafi.example.flatmemberrecyclerview
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
--- app/src/main/java/shafi/example/flatmemberrecyclerview/MainActivity.kt
+++ app/src/main/java/shafi/example/flatmemberrecyclerview/MainActivity.kt
@@ -1,19 +1,31 @@
 package shafi.example.flatmemberrecyclerview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import shafi.example.flatmemberrecyclerview.R
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val memberRV = findViewById<RecyclerView>(R.id.memberRV)
-        val memberAdapter = MemberAdapter(this, Member.memberArrayList())
-        val linearLayoutManager = LinearLayoutManager(this)
-        memberRV.layoutManager = linearLayoutManager
-        memberRV.adapter = memberAdapter
+
+        GlobalScope.launch {
+            delay(3000)
+            intentToNavHostActivity()
+        }
+
+    }
+
+    private fun intentToNavHostActivity() {
+        val intent = Intent(this, NavHostActivity::class.java)
+        startActivity(intent)
     }
 }
